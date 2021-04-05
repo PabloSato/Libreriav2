b4bff54ee85eaeb7eb87bc086d31ba60ff0e3f23
Pablo Sato <pablofersato@gmail.com>
2021-04-05 08:45:47 +0200

    termino clase donde

--- Bibliov2-1/src/biblio/ubi/Donde.java
+++ Bibliov2-1/src/biblio/ubi/Donde.java
@@ -1,5 +1,32 @@
 package biblio.ubi;
 
-public class Donde {
+public abstract class Donde {
+
+	// PROPIEDADES
+	private String nombre;
+
+	// CONSTRUCTOR
+	public Donde(String nombre) {
+		this.nombre = nombre;
+	}
+
+	// METODOS
+	public abstract String detalle();
+
+	public abstract String detalles();
+
+	@Override
+	public String toString() {
+		return "Donde [nombre=" + nombre + "]";
+	}
+
+	// GETTER Y SETTER
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
 }
