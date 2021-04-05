7d01933b36a78e3d404f8d84dc67ac7aa00bee17
Pablo Sato <pablofersato@gmail.com>
2021-04-05 09:17:28 +0200

    add clase saga

--- Bibliov2-1/src/biblio/colec/Saga.java
+++ Bibliov2-1/src/biblio/colec/Saga.java
@@ -1,5 +1,35 @@
 package biblio.colec;
 
-public class Saga {
+import java.util.ArrayList;
 
+public class Saga extends Compilacion {
+
+	// PROPIEDADES
+	private ArrayList<Tomo> tomos;
+
+	// CONSTRUCTOR
+	public Saga(String nombre) {
+		super(nombre);
+		this.tomos = new ArrayList<Tomo>();
+	}
+	//METODOS
+	@Override
+	public String detalle() {
+		return super.getNombre()+" ";
+	}
+	@Override
+	public String toString() {
+		return "Saga [tomos=" + tomos + "]";
+	}
+	//GETTER Y SETTER
+	public ArrayList<Tomo> getTomos() {
+		return tomos;
+	}
+
+	public void setTomos(ArrayList<Tomo> tomos) {
+		this.tomos = tomos;
+	}
+	
+	
+	
 }
