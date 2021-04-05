9411026c070d4a2dbab62c7051ce9cadb706a64f
Pablo Sato <pablofersato@gmail.com>
2021-04-05 09:24:59 +0200

    add clase abstracta obra

---
+++ Bibliov2-1/src/biblio/obra/Obra.java
@@ -0,0 +1,79 @@
+package biblio.obra;
+
+public abstract class Obra {
+
+	// PROPIEDADES
+	private String titulo;
+	private int paginas;
+	private String genero;
+	private String idioma;
+	private boolean leido;
+
+	// CONSTRUCTOR
+	public Obra(String titulo, int paginas, String genero, String idioma, boolean leido) {
+		this.titulo = titulo;
+		this.paginas = paginas;
+		this.genero = genero;
+		this.idioma = idioma;
+		this.leido = leido;
+	}
+
+	public Obra() {
+		this.titulo = "";
+		this.paginas = 0;
+		this.genero = "";
+		this.idioma = "";
+		this.leido = false;
+	}
+
+	// METODOS
+	public abstract String consulta();
+
+	@Override
+	public String toString() {
+		return "Obra [titulo=" + titulo + ", paginas=" + paginas + ", genero=" + genero + ", idioma=" + idioma
+				+ ", leido=" + leido + "]";
+	}
+
+	// GETTER Y SETTER
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public int getPaginas() {
+		return paginas;
+	}
+
+	public void setPaginas(int paginas) {
+		this.paginas = paginas;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getIdioma() {
+		return idioma;
+	}
+
+	public void setIdioma(String idioma) {
+		this.idioma = idioma;
+	}
+
+	public boolean isLeido() {
+		return leido;
+	}
+
+	public void setLeido(boolean leido) {
+		this.leido = leido;
+	}
+
+}
