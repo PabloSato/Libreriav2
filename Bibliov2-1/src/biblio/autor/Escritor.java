29ea531994df085a69669585a90b9ac31d03a3ef
Pablo Sato <pablofersato@gmail.com>
2021-04-05 19:10:35 +0200

    dicionario y metodos add empiezo,Q PASA CON AUTOR?

--- Bibliov2-1/src/biblio/autor/Escritor.java
+++ Bibliov2-1/src/biblio/autor/Escritor.java
@@ -7,30 +7,39 @@ import biblio.obra.Libro;
 public class Escritor extends Autor {
 
 	// PROPIEDADES
-
+	private boolean escritor;
 	private ArrayList<Libro> libros;
 
 	// CONSTRUCTOR
-	public Escritor(String nombre, String apellidos, String alias, String bio) {
+	public Escritor(String nombre, String apellidos, String alias, String bio, boolean escritor) {
 		super(nombre, apellidos, alias, bio);
+		this.escritor = escritor;
 		this.libros = new ArrayList<Libro>();
 	}
-	
-	//METODOS
+
+	// METODOS
 	@Override
 	public String consulta() {
 		return super.getAlias();
 	}
-	
-	
+
 	@Override
 	public String toString() {
 		return "Escritor [libros=" + libros + "]";
 	}
 
-	//GETTER Y SETTER
+	// GETTER Y SETTER
+
 	public ArrayList<Libro> getLibros() {
 		return libros;
+	}
+
+	public boolean isEscritor() {
+		return escritor;
+	}
+
+	public void setEscritor(boolean escritor) {
+		this.escritor = escritor;
 	}
 
 	public void setLibros(ArrayList<Libro> libros) {
--- Bibliov2-1/src/biblio/bbdd/MetodosAdd.java
+++ Bibliov2-1/src/biblio/bbdd/MetodosAdd.java
@@ -1,5 +1,150 @@
 package biblio.bbdd;
 
+import java.sql.Connection;
+import java.util.Scanner;
+
+import biblio.autor.Autor;
+
 public class MetodosAdd {
+
+	private static Scanner scan = new Scanner(System.in);
+
+	// CONSTRUCTOR
+	public MetodosAdd() {
+
+	}
+
+	// NO FIN!!!!!
+
+	// METODOS ADD
+	// ------------------------------------------------------------------LIBRO
+	// ------------------------------------------------------------------AUTOR
+	public Autor addAutor(Connection con) {
+		String nombre = "", apellidos = "", alias = "", bio = "";
+		String siNo, min;
+		boolean escritor = false, dibujante = false, flag = false;
+		;
+
+		do {
+			System.out.println("¡Vamos a Añadir un nuevo Autor!");
+			do {
+				try {
+					System.out.println("Introduce el Nombre:");
+					nombre = scan.nextLine();
+					if (nombre.equals("")) {
+						System.out.println("¡No te Olvides del Nombre!");
+						flag = false;
+					} else {
+						flag = true;
+					}
+				} catch (Exception e) {
+					System.out.println("Valores no aceptados");
+					System.out.println(e.toString());
+					flag = false;
+				}
+			} while (flag != true);
+			do {
+				try {
+					System.out.println("Introduce los Apellidos: ");
+					apellidos = scan.nextLine();
+					if (apellidos.equals("")) {
+						System.out.println("¡No te olvides de los Apellidos!");
+						flag = false;
+					} else {
+						flag = true;
+					}
+				} catch (Exception e) {
+					System.out.println("Valores no aceptados");
+					System.out.println(e.toString());
+					flag = false;
+				}
+			} while (flag != true);
+			do {
+				try {
+					System.out.println("Introduce el Alias:");
+					alias = scan.nextLine();
+					if (alias.equals("")) {
+						System.out.println("¡No te olvides del Alias!");
+						flag = false;
+					} else {
+						flag = true;
+					}
+				} catch (Exception e) {
+					System.out.println("Valores no aceptados");
+					System.out.println(e.toString());
+					flag = false;
+				}
+			} while (flag != true);
+			do {
+				try {
+					System.out.println("Introduce la Biografía: ");
+					bio = scan.nextLine();
+					flag = true;
+				} catch (Exception e) {
+					System.out.println("Valores no aceptados");
+					System.out.println(e.toString());
+					flag = false;
+				}
+			} while (flag != true);
+			do {
+				try {
+					System.out.println("¿Es escritor? (Si/No)");
+					siNo = scan.nextLine();
+					if (siNo.equals("")) {
+						flag = false;
+					} else {
+						min = siNo.toLowerCase().trim();
+						if (min.equals("si")) {
+							escritor = true;
+							flag = true;
+						} else if (min.equals("no")) {
+							escritor = false;
+							flag = true;
+						} else {
+							System.out.println("Contesta si o no");
+							flag = false;
+						}
+					}
+					flag = true;
+				} catch (Exception e) {
+					System.out.println("Valores no aceptados");
+					System.out.println(e.toString());
+					flag = false;
+				}
+			} while (flag != true);
+			do {
+				try {
+					System.out.println("¿Es dibujante? (Si/No)");
+					siNo = scan.nextLine();
+					if (siNo.equals("")) {
+						flag = false;
+					} else {
+						min = siNo.toLowerCase().trim();
+						if (min.equals("si")) {
+							dibujante = true;
+							flag = true;
+						} else if (min.equals("no")) {
+							dibujante = false;
+							flag = true;
+						} else {
+							System.out.println("Contesta si o no");
+							flag = false;
+						}
+					}
+					flag = true;
+				} catch (Exception e) {
+					System.out.println("Valores no aceptados");
+					System.out.println(e.toString());
+					flag = false;
+				}
+			} while (flag != true);
+		} while (flag != true);
+
+		if (escritor != false) {
+			
+		}
+		return autor;
+	}
+	// ------------------------------------------------------------------LIBRO
 
 }
