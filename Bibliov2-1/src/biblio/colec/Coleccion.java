ec8cd652802c68ea36191e27a939f92c458452c1
Pablo Sato <pablofersato@gmail.com>
2021-04-05 09:20:25 +0200

    add clase coleccion

---
+++ Bibliov2-1/src/biblio/colec/Coleccion.java
@@ -0,0 +1,45 @@
+package biblio.colec;
+
+import java.util.ArrayList;
+
+public class Coleccion extends Compilacion{
+	
+	//PROPIEDADES
+	private ArrayList<Saga> sagas;
+	private ArrayList<Tomo> tomos;
+	
+	//CONSTRUCTOR
+	public Coleccion(String nombre) {
+		super(nombre);
+		this.sagas = new ArrayList<Saga>();
+		this.tomos = new ArrayList<Tomo>();
+	}
+	//METODOS
+	@Override
+	public String detalle() {
+		return super.getNombre()+" ";
+	}
+	@Override
+	public String toString() {
+		return "Coleccion [sagas=" + sagas + ", tomos=" + tomos + "]";
+	}
+	//GETTER Y SETTER
+	public ArrayList<Saga> getSagas() {
+		return sagas;
+	}
+	
+	public void setSagas(ArrayList<Saga> sagas) {
+		this.sagas = sagas;
+	}
+
+	public ArrayList<Tomo> getTomos() {
+		return tomos;
+	}
+
+	public void setTomos(ArrayList<Tomo> tomos) {
+		this.tomos = tomos;
+	}
+	
+	
+
+}
